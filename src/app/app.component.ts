768fa5160ccceddc3d81b1697959550d61a3f8b8
Reshma Hussain <reshnaz91@gmail.com>
2017-12-26 13:09:42 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   // Declaring variables
   firstName:string;
   lastName:string;
